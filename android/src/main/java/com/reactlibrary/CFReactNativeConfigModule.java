7408f1fe4e9bb844ec80898bbffaaa89a39ca4b0
Amandeep Singh <>
2019-02-04 18:20:47 +0530

    Add get constants method

--- android/src/main/java/com/reactlibrary/CFReactNativeConfigModule.java
+++ android/src/main/java/com/reactlibrary/CFReactNativeConfigModule.java
@@ -1,10 +1,15 @@
 
 package com.reactlibrary;
 
+import android.content.Context;
+import android.content.res.Resources;
+import android.util.Log;
+
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
-import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CFReactNativeConfigModule extends ReactContextBaseJavaModule {
 
@@ -17,6 +22,38 @@ public class CFReactNativeConfigModule extends ReactContextBaseJavaModule {
 
   @Override
   public String getName() {
-    return "CFCfReactNativeConfig";
+    return "CFReactNativeConfig";
   }
+
+    @Override
+    public Map<String, Object> getConstants() {
+        final Map<String, Object> constants = new HashMap<>();
+
+        try {
+            Context context = getReactApplicationContext();
+            int resId = context.getResources().getIdentifier("build_config_package", "string", context.getPackageName());
+            String className;
+            try {
+                className = context.getString(resId);
+            } catch (Resources.NotFoundException e) {
+                className = getReactApplicationContext().getApplicationContext().getPackageName();
+            }
+            Class clazz = Class.forName(className + ".BuildConfig");
+            Field[] fields = clazz.getDeclaredFields();
+            for(Field f: fields) {
+                try {
+                    constants.put(f.getName(), f.get(null));
+                }
+                catch (IllegalAccessException e) {
+                    Log.d("ReactNative", "ReactConfig: Could not access BuildConfig field " + f.getName());
+                }
+            }
+        }
+        catch (ClassNotFoundException e) {
+            Log.d("ReactNative", "ReactConfig: Could not find BuildConfig class");
+        }
+
+        return constants;
+    }
 }
+
